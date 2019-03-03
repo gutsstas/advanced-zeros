22717a29510bea1e970f1e3c691385041b7dad4f
gutsstas <tayson_1200@mail.ru>
2019-03-03 13:44:29 +0300

    fead: add function advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  let count = 0;
+  var arr = new Array();
+  var arr1 = new Array();
+  var arr2 = new Array();
+  var j = 0;
+  var i = 2;
+  do{
+    if (base % i == 0) {
+      arr[j] = i;
+      j++;
+      base = base / i;
+    }
+    else i++;
+  }
+  while (i <= base);
+  let zeros = 0;
+  let num = number;
+  for (let z = 0; z < arr.length; z++) {
+    while (num > arr[z]){
+      num/= arr[z];
+      zeros+=Math.floor(num)
+    }
+      arr1.push(zeros);
+      zeros = 0;
+      num = number;
+  }
+  let element;
+  for (let y = 0; y < arr1.length; y++) {
+
+      for (let j = 0; j < arr1.length; j++) {
+        if (arr1[j] == arr1[y]) count++;
+      }
+      element = arr1[y]/count;
+      count = 0;
+      arr2.push(Math.floor(element));
+    }
+    Array.min = function( array ){
+      return Math.min.apply( Math, array );
+    }
+    return Array.min(arr2);
 }
